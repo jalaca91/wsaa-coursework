e67f2011592601b36becec961c6024feaa425aa0
Jaime Lara Carrillo <G00425663@atu.ie>
2024-03-19 17:22:42 +0000

    Update

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -7,3 +7,27 @@ import requests
 from github import Github
 from config import config as cfg 
 
+# APIKey from config.py
+apikey = cfg["githubkey"]  
+g = Github(apikey)
+
+# Get clone url
+repo = g.get_repo("jalaca91/WSAA-coursework")
+
+# Get downloadurl
+fileInfo = repo.get_contents("assignments/text_assignment04.txt")
+urlOfFile = fileInfo.download_url
+
+# Making a http request from the downloadurl 
+response = requests.get(urlOfFile)
+contentOfFile = response.text
+
+# Replacing instances of "Andrew" with "Jaime"
+newContents = contentOfFile.replace("Andrew", "Jaime")
+print(newContents)
+      
+# Updating the file and pushing back to repository
+gitHubResponse=repo.update_file(fileInfo.path,"updatedtest_text_assignment04.txt",newContents,fileInfo.sha)
+print(gitHubResponse)
+
+
